921a8cf64a394d1f253c0036f88f3c256cfabf17
Alejandrorodram <ale1.zul@gmail.com>
2022-03-08 09:27:53 +0000

    first commit

---
+++ HighestAndLowestNumber/Kata.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace HighestAndLowestNumber
+{
+    internal class Kata
+    {
+        public static string HighestAndLowestNumber1(string numbers)
+        {
+            var parsed = numbers.Split().Select(int.Parse);
+            return parsed.Max() + " " + parsed.Min();
+        }
+        public static string HighestAndLowestNumber2(string numbers)
+        {
+            var numbersList = numbers.Split(' ').Select(x => Convert.ToInt32(x));
+            return string.Format("{0} {1}", numbersList.Max(), numbersList.Min());
+        }
+        public static string HighestAndLowestNumber3(string numbers)
+        {
+            int min = int.MaxValue;
+            int max = int.MinValue;
+            string[] values = numbers.Split(' ');
+            for (int i = values.Length - 1; i >= 0; --i)
+            {
+                int value = System.Convert.ToInt32(values[i]);
+                if (value < min)
+                {
+                    min = value;
+                }
+                if (value > max)
+                {
+                    max = value;
+                }
+            }
+            return string.Format("{0} {1}", max, min);
+        }
+    }
+}
---
+++ HighestAndLowestNumber/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HighestAndLowestNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.HighestAndLowestNumber1("5 2 3 1"));
+            Console.WriteLine(Kata.HighestAndLowestNumber2("5 2 3 1"));
+            Console.WriteLine(Kata.HighestAndLowestNumber3("5 2 3 1"));
+
+        }
+    }
+}
